ea5184c60b457f26e535fcd794e077c598ed4c7a
Priyanshu <assudanipriyanshu@gmail.com>
2024-11-12 20:29:06 +0530

    commit

--- src/components/FoodListItem.tsx
+++ src/components/FoodListItem.tsx
@@ -1,27 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 
 const FoodListItem = ({ item }) => {
-    return (
-      <View
-        style={{
-          backgroundColor: "gainsboro",
-          padding: 10,
-          borderRadius: 5,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <View style={{ flex: 1, gap: 5 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.label}</Text>
-          <Text style={{ color: "dimgray" }}>
-            {item.cal} cal, {item.brand}
-          </Text>
-        </View>
-        <AntDesign name="pluscircleo" size={24} color="royalblue" />
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1, gap: 5 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.label}</Text>
+        <Text style={{ color: "dimgray" }}>
+          {item.cal} cal, {item.brand}
+        </Text>
       </View>
-    );
-  };
+      <AntDesign name="pluscircleo" size={24} color="royalblue" />
+    </View>
+  );
+};
 
-  export default FoodListItem
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "gainsboro",
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export default FoodListItem;
