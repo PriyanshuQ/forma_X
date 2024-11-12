ea033516d04faac12e12af7cc4c91afae31fe446
Priyanshu <assudanipriyanshu@gmail.com>
2024-11-12 20:24:51 +0530

    Food List Item

---
+++ src/app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import FoodListItem from "../components/FoodListItem";
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Food Item View */}
+      <FoodListItem item={{ label: "Pizza", cal: 75, brand: "Dominos" }} />
+      <FoodListItem item={{ label: "Apple", cal: 50, brand: "Generic" }} />
+      {/* <FoodListItem /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 10,
+    gap: 5,
+  },
+});
